fbe02a1e85a095cfbf11b02b18010eb8ef6a1b34
Manuel Herold <herrytco@gmail.com>
2020-06-16 15:47:52 +0200

    face recognition
    added face recognition to detect the rectangles containing faces in the image to crop the are to the face only

--- preprocessing/cropper.py
+++ preprocessing/cropper.py
@@ -1,17 +1,17 @@
 import os
+import cv2
 from PIL import Image
 
 target_width = 32
 target_height = 32
+face_padding = 0.2
 first = True
 
-def crop(filename):
+def scaleDown(filename):
     '''
-    Resizes and crops the image
+    resizes the image to the global target_* values 
     :param filename: name of the image relative to /images/
     '''
-
-    print("cropping: "+filename)
 
     im = Image.open('images/'+filename)
 
@@ -25,28 +25,85 @@ def crop(filename):
         width_new = height_new * width / height
 
     im = im.resize((int(width_new), int(height_new)), Image.ANTIALIAS)
+    im.save('images/'+filename)
 
-    # crop image to target_height x target_width
-    width,height = im.size
-    if width < height:
-        diff = height - width
+def detect(filename):
+    # Create the haar cascade
+    faceCascade = cv2.CascadeClassifier('preprocessing/haarcascade_frontalface_default.xml')
 
-        # top, left, right, bottom
-        crop_box = (0, target_width, diff / 2, diff / 2 + target_height)
-    else:
-        diff = width - height
+    # convert to grayscale
+    image = cv2.imread('images/'+filename)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-        crop_box = (diff / 2, diff / 2 + target_width, 0, target_height)
+    faces = faceCascade.detectMultiScale(gray, 1.1, 4)
+
+    # print("Found {0} faces!".format(len(faces)))
+
+    c = None
+
+    for (x, y, w, h) in faces:
+        w_padding = int(w * face_padding)
+        h_padding = int(h * face_padding)
+
+        x -= w_padding
+        w += 2 * w_padding
+
+        y -= h_padding
+        h += 2 * h_padding
+
+        # squarify the rectangle
+        if h > w:
+            # x -= int((h-w)/2)
+            w = h
+        else:
+            y -= int((w-h)/2)
+            h = w
+
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        # center point of the face-rectangle
+        c = (x,y,w,h)
+
+    # cv2.imshow("Faces found", image)
+    # cv2.waitKey(0)
     
+    if len(faces) == 1:
+        return c
+    
+    raise AssertionError("Image does not contain any faces!")
+
+def squarify(filename, coi):
+    im = Image.open('images/'+filename)
+
+    a = coi[3]
+
+    # left, top, right, bottom
+    crop_box = (
+        coi[0],
+        coi[1],
+        coi[0] + a,
+        coi[1] + a
+    )
+
     im = im.crop(crop_box)
 
     # save the final result
     im.save('images/'+filename)
-
 
 if __name__ =="__main__":
     if not os.path.exists('images'):
         os.mkdir('images')
     
     for filename in os.listdir(r'images'):
-        crop(filename)
+        try:
+            print("preprocessing "+filename)
+            c = detect(filename)
+            squarify(filename, c)
+            scaleDown(filename)
+            detect(filename)
+            
+        except AssertionError:
+            os.remove('images/'+filename)
+            print("no faces found, deleting image")
+
+        # crop(filename)
