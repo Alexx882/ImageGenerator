9bf15887a2c62e2485139fccf6bf451b11b45b42
Manuel <herrytco@gmail.com>
2020-07-01 12:11:37 +0200

    offset + nImages in downloader
    adjustable offset and number of images added to downloader

--- crawler/downloader.py
+++ crawler/downloader.py
@@ -2,6 +2,12 @@ import requests
 import os
 import shutil
 
+# number of images the downloader should download
+N_IMAGES = 999999999
+
+# number of images that should be skipped before counting
+OFFSET = 0
+
 if __name__ == '__main__':
     if os.path.exists('images'):
         shutil.rmtree('images')
@@ -11,7 +17,11 @@ if __name__ == '__main__':
     with open('crawler/sources.txt', 'r') as file:
         sources = file.readlines()
     
-    for i in range(len(sources)):
+    for i in range(min(len(sources), N_IMAGES+OFFSET)):
+        if i < OFFSET:
+            continue
+
+        print("downloading: "+sources[i])
         response = requests.get(sources[i])
 
         with open(f'images/{i}.png', 'wb') as file:
