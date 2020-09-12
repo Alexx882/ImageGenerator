779930b97e9b08d7437a3ed8fa91226f34b88d18
Alexander Lercher <allercher@edu.aau.at>
2020-09-12 18:36:40 +0200

    Added possibility to store images in memory instead of on disk

--- preprocessing/cropper.py
+++ preprocessing/cropper.py
@@ -1,19 +1,16 @@
 import os
 import cv2
 from PIL import Image
+import numpy as np
 
 target_width = 32
 target_height = 32
 face_padding = 0.2
 first = True
 
-def scaleDown(filename):
-    '''
-    resizes the image to the global target_* values 
-    :param filename: name of the image relative to /images/
-    '''
-
-    im = Image.open('images/'+filename)
+def scale_down(image: Image) -> Image:
+    '''resizes the image to the global target_* values'''
+    im = image
 
     # scale the smaller side to 'target' px
     width,height = im.size
@@ -25,24 +22,24 @@ def scaleDown(filename):
         width_new = height_new * width / height
 
     im = im.resize((int(width_new), int(height_new)), Image.ANTIALIAS)
-    im.save('images/'+filename)
+    return im
 
-def detect(filename):
-    '''Detects the face for an image.'''
+def detect_face(image: 'cv2.Mat') -> '(x,y,w,h)':
+    '''Detects the face bounding box for an image.'''
 
     # Create the haar cascade
     faceCascade = cv2.CascadeClassifier('preprocessing/haarcascade_frontalface_default.xml')
 
     # convert to grayscale
-    image = cv2.imread('images/'+filename)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     faces = faceCascade.detectMultiScale(gray, 1.1, 4)
 
     # print("Found {0} faces!".format(len(faces)))
 
-    c = None
+    assert len(faces) == 1, "Image does not contain one distinct face!"
 
+    # TODO remove loop (kept to reduce git changes)
     for (x, y, w, h) in faces:
         w_padding = int(w * face_padding)
         h_padding = int(h * face_padding)
@@ -65,18 +62,13 @@ def detect(filename):
 
         # center point of the face-rectangle
         c = (x,y,w,h)
+        return c
 
     # cv2.imshow("Faces found", image)
     # cv2.waitKey(0)
-    
-    if len(faces) == 1:
-        return c
-    
-    raise AssertionError("Image does not contain one distinct face!")
 
-def squarify(filename, coi):
-    im = Image.open('images/'+filename)
-
+def squarify(image: Image, coi) -> Image:
+    im = image
     a = coi[3]
 
     # left, top, right, bottom
@@ -88,28 +80,42 @@ def squarify(filename, coi):
     )
 
     im = im.crop(crop_box)
+    return im
 
-    # save the final result
-    im.save('images/'+filename)
+def pil_to_opencv_image(pil_image: Image) -> 'cv2.Mat':
+    open_cv_image = np.array(pil_image) 
+    # Convert RGB to BGR 
+    open_cv_image = open_cv_image[:, :, ::-1].copy() 
+    return open_cv_image
+
+def crop_image_to_face(filename):
+    try:
+        # print("preprocessing "+filename)
+        im = Image.open('images/'+filename)
+
+        cv_im = pil_to_opencv_image(im)
+        c = detect_face(cv_im)
+
+        im = squarify(im, c)
+        im = scale_down(im)
+
+        cv_im = pil_to_opencv_image(im)
+        detect_face(cv_im)
+
+        im.save('images/'+filename)
+            
+    except AssertionError:
+        os.remove('images/'+filename)
+        # print("no distinct face found, deleting image")
 
 def crop_to_face():
     '''Crops all images in folder ./images/ to their faces.'''
     if not os.path.exists('images'):
-        os.mkdir('images')
+        print('Folder does not exist.')
+        return
     
     for filename in os.listdir('images'):
-        try:
-            # print("preprocessing "+filename)
-            c = detect(filename)
-            squarify(filename, c)
-            scaleDown(filename)
-            detect(filename)
-            
-        except AssertionError:
-            os.remove('images/'+filename)
-            # print("no distinct face found, deleting image")
-
-        # crop(filename)
+       crop_image_to_face(filename)
 
 
 if __name__ =="__main__":
