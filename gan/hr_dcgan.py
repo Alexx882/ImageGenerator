d21430b44454b044ed072c0dbdf54371c92a3f52
Alexander Lercher <allercher@edu.aau.at>
2020-09-15 23:28:07 +0200

    [HR-DCGAN] new architecture for higher resolution

--- gan/hr_dcgan.py
+++ gan/hr_dcgan.py
@@ -23,40 +23,63 @@ class HR_DCGAN(GAN):
         noise_shape = (100,)
 
         model = Sequential([
-            layers.Dense(7*7*256, use_bias=False, input_shape=noise_shape),
+            layers.Dense(8*8*256, use_bias=False, input_shape=noise_shape),
             layers.BatchNormalization(),
             layers.ELU(),
-            layers.Reshape((7,7,256)),
-            # shape (7,7,256)
+            layers.Reshape((8,8,256)),
+            # shape (8,8,256)
 
             layers.Conv2DTranspose(128, (4,4), strides=(1,1), padding='same', use_bias=False),
             layers.BatchNormalization(),
             layers.ELU(),
-            # shape (7,7,128)
+            # shape (8,8,128)
 
             # stride 2 -> larger image
-            # thiccness 128 -> channels
+            # thiccness 64 -> channels
             layers.Conv2DTranspose(64, (4,4), strides=(2,2), padding='same', use_bias=False),
             layers.BatchNormalization(),
             layers.ELU(),
-            # shape (14,14,64)
+            # shape (16,16,64)
+
+            layers.Conv2DTranspose(32, (4,4), strides=(2,2), padding='same', use_bias=False),
+            layers.BatchNormalization(),
+            layers.ELU(),
+            # shape (32,32,32)
+
+            layers.Conv2DTranspose(16, (4,4), strides=(2,2), padding='same', use_bias=False),
+            layers.BatchNormalization(),
+            layers.ELU(),
+            # shape (64,64,16)
+
+            layers.Conv2DTranspose(8, (4,4), strides=(2,2), padding='same', use_bias=False),
+            layers.BatchNormalization(),
+            layers.ELU(),
+            # shape (128,128,8)
 
             layers.Conv2DTranspose(1, (4,4), strides=(2,2), padding='same', use_bias=False, activation='tanh')
-            # shape (28,28,1)
+            # shape (256,256,1)
 
         ])
 
         return model
 
     def build_discriminator(self):
-        img_shape = (28, 28, 1)
+        img_shape = (256, 256, 1)
         
         model = Sequential([
 
-            layers.Conv2D(64, 4, strides=(2,2), padding='same', input_shape=img_shape),
+            layers.Conv2D(32, 4, strides=(2,2), padding='same', input_shape=img_shape),
+            layers.ELU(),
+
+            layers.Conv2D(64, 4, strides=(2,2), padding='same'),
+            layers.BatchNormalization(),
             layers.ELU(),
 
             layers.Conv2D(128, 4, strides=(2,2), padding='same'),
+            layers.BatchNormalization(),
+            layers.ELU(),
+
+            layers.Conv2D(256, 4, strides=(2,2), padding='same'),
             layers.BatchNormalization(),
             layers.ELU(),
 
